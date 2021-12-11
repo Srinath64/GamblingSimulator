3196fe6d35b29ab0bf8407e7fc299746c8117bf3
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-11 15:40:16 +0530

    First commit

---
+++ Gambler.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Gambler {
+
+    public static void main(String[] args) {
+
+        int stake = Integer.parseInt(args[0]);
+        int goal = Integer.parseInt(args[1]);
+        int trials = Integer.parseInt(args[2]);
+
+        int bets = 100;
+        int wins = 0;
+
+    }
+}
