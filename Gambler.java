f3481cad2354735a58101e7d1cc56d9a0024af2e
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 12:36:10 +0530

    Added UC3

--- Gambler.java
+++ Gambler.java
@@ -2,23 +2,23 @@ package com.company;
 
 public class Gambler {
 
-    int stake = 100;
+    //int stake = 100;
     int LOSE = 0;
     int WIN = 1;
 
     public static void main (String[] args) {
          Gambler gambler = new Gambler();
-         int trial = (int) Math.floor(Math.random()* 10 ) % 2;
+         int stake = (int) Math.floor(Math.random()* 10 ) % 2;
 
-        if ( trial == 0){
-            gambler.stake = gambler.stake -1;
-            System.out.println(" "+gambler.stake);
-        }
-        else
-        {
-            gambler.stake = gambler.stake +1;
-            System.out.println(" "+gambler.stake);
-        }
+       if (stake == stake/2){
+            System.out.println("Exit from the Table");
+       }
+       else
+       {
+           System.out.println("Continue to Place bets");
+       }
+
+
     }
 
     }
