878b761c7d77aba1af9864b85a4b35f9f685e703
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 14:19:40 +0530

    Added UC6

--- Gambler.java
+++ Gambler.java
@@ -5,19 +5,25 @@ import java.util.Random;
 public class Gambler {
     static int Every_Day_Stack=100;
     final static int Bet_Every_Game=1;
-    public static void main(String[] args) {
+    static int WinDays=0;
+    static int LossDays=0;
+    static int Win=0;
+    static int Loss=0;
+    static int winMax=0;
+    static int lossMax=0;
+    static int luckyDay=0;
+    static int unluckyDay=0;
+
+    public static void main(String[] args){
         System.out.println("Welcome To Gambler Simulator");
-        System.out.println("Every Day Stack: "+Every_Day_Stack);
-        System.out.println("Beat Price: "+Bet_Every_Game);
 
         int Stack_Upper = 150;
         int Stack_Lower = 50;
         int Winning = 0;
-        int WinDays=0;
-        int LossDays=0;
 
         for(int days = 1; days <= 30; days++) {
             Every_Day_Stack=100;
+
             while(Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
                 Random random = new Random();
                 int bet = random.nextInt(2);
@@ -25,12 +31,15 @@ public class Gambler {
                 switch(bet) {
                     case 1:
                         Every_Day_Stack += Bet_Every_Game;
+                        Win++;
                         break;
                     default:
                         Every_Day_Stack -= Bet_Every_Game;
+                        Loss++;
                         break;
                 }
             }
+
             if(Every_Day_Stack == Stack_Upper) {
                 WinDays += 1;
                 Winning += 50;
@@ -38,10 +47,27 @@ public class Gambler {
                 LossDays += 1;
                 Winning -= 50;
             }
+
+            if(Win > winMax) {
+                winMax = Win;
+                luckyDay = days + 1;
+            }
+
+            if(Loss > lossMax) {
+                lossMax = Loss;
+                unluckyDay = days + 1;
+            }
+
+            Win=0;
+            Loss=0;
         }
+        System.out.println("The Luckiest Day is :"+luckyDay);
+        System.out.println("The Unluckiest Day is: "+unluckyDay);
         System.out.println("Total Win Days: "+WinDays);
         System.out.println("Total Loss Days: "+LossDays);
         System.out.println("Total Win Price Won In Month Is " +Winning + "\n");
     }
 }
+
+
 
