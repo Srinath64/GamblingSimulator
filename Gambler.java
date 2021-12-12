488f7c496c84412e4dc3b1dd7069a1ed5f42aa09
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 14:00:18 +0530

    Added UC4

--- Gambler.java
+++ Gambler.java
@@ -5,25 +5,38 @@ import java.util.Random;
 public class Gambler {
     static int Every_Day_Stack=100;
     final static int Bet_Every_Game=1;
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome To Gambler Simulator");
         System.out.println("Every Day Stack: "+Every_Day_Stack);
         System.out.println("Beat Price: "+Bet_Every_Game);
 
         int Stack_Upper = 150;
-        int Stack_Lower =50;
+        int Stack_Lower = 50;
+        int Winning = 0;
 
-        while(Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
-            Random random = new Random();
-            int bet = random.nextInt(2);
+        for(int days = 1; days <= 20; days++) {
+            Every_Day_Stack=100;
+            while(Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
+                Random random = new Random();
+                int bet = random.nextInt(2);
 
-            if(bet == 1) {
-                System.out.println("Bet Win");
-                System.out.println("Final Amount: "+(Every_Day_Stack += Bet_Every_Game));
-            }else {
-                System.out.println("Bet Loss");
-                System.out.println("Final Amount: "+(Every_Day_Stack -= Bet_Every_Game));
+                switch(bet) {
+                    case 1:
+                        Every_Day_Stack += Bet_Every_Game;
+                        break;
+                    default:
+                        Every_Day_Stack -= Bet_Every_Game;
+                        break;
+                }
             }
+            if(Every_Day_Stack == Stack_Upper) {
+                System.out.println("Player Has Won Gambler For Day "+days);
+                Winning += 50;
+            }else {
+                System.out.println("Player Has Loss Gambler For Day "+days);
+                Winning -= 50;
+            }
+            System.out.println("Total Win Price: "+Winning + "\n");
         }
     }
 }
