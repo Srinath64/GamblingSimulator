47fed5577cd17d25f99b080a32c50dee4ffb0d9d
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 13:13:34 +0530

    Added UC3

--- Gambler.java
+++ Gambler.java
@@ -2,24 +2,16 @@ package com.company;
 
 public class Gambler {
 
-    //int stake = 100;
-    int LOSE = 0;
-    int WIN = 1;
+    final int WON =1;
+    final int LOSS = 0;
+    final int STAKE = 100;
+    final int BET_AMOUNT = 1;
+    final int DAILY_RESIGN_PERC = 50;
 
-    public static void main (String[] args) {
-         Gambler gambler = new Gambler();
-         int stake = (int) Math.floor(Math.random()* 10 ) % 2;
+    int dailyBetAmount = STAKE;
+    int maxTotalDailyAmount = ((STAKE + STAKE * DAILY_RESIGN_PERC / 100));
+    int minTotalDailyAmount = ((STAKE - STAKE * DAILY_RESIGN_PERC / 100));
 
-       if (stake == stake/2){
-            System.out.println("Exit from the Table");
-       }
-       else
-       {
-           System.out.println("Continue to Place bets");
-       }
-
-
-    }
 
     }
 
