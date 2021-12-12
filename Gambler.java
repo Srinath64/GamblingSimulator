f6d2c0b58ddbe8a6e51b8ea1d3e2dd23f4f79211
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 13:55:59 +0530

    Added UC3

--- Gambler.java
+++ Gambler.java
@@ -1,17 +1,30 @@
 package com.company;
 
+import java.util.Random;
+
 public class Gambler {
+    static int Every_Day_Stack=100;
+    final static int Bet_Every_Game=1;
+    public static void main (String[] args){
+        System.out.println("Welcome To Gambler Simulator");
+        System.out.println("Every Day Stack: "+Every_Day_Stack);
+        System.out.println("Beat Price: "+Bet_Every_Game);
 
-    final int WON =1;
-    final int LOSS = 0;
-    final int STAKE = 100;
-    final int BET_AMOUNT = 1;
-    final int DAILY_RESIGN_PERC = 50;
+        int Stack_Upper = 150;
+        int Stack_Lower =50;
 
-    int dailyBetAmount = STAKE;
-    int maxTotalDailyAmount = ((STAKE + STAKE * DAILY_RESIGN_PERC / 100));
-    int minTotalDailyAmount = ((STAKE - STAKE * DAILY_RESIGN_PERC / 100));
+        while(Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
+            Random random = new Random();
+            int bet = random.nextInt(2);
 
-
+            if(bet == 1) {
+                System.out.println("Bet Win");
+                System.out.println("Final Amount: "+(Every_Day_Stack += Bet_Every_Game));
+            }else {
+                System.out.println("Bet Loss");
+                System.out.println("Final Amount: "+(Every_Day_Stack -= Bet_Every_Game));
+            }
+        }
     }
+}
 
