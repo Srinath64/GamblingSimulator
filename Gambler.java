3dbf2d26ab23f5bc101ebd57c88911e6b78cb91c
Srinath S <srinath@Srinaths-Air.domain.name>
2021-12-12 12:38:03 +0530

    Added UC1

--- Gambler.java
+++ Gambler.java
@@ -4,12 +4,8 @@ public class Gambler {
 
     public static void main(String[] args) {
 
-        int stake = Integer.parseInt(args[0]);
-        int goal = Integer.parseInt(args[1]);
-        int trials = Integer.parseInt(args[2]);
-
-        int bets = 100;
-        int wins = 0;
+        int STAKE = 100;
+        int BET = 1;
 
     }
 }
